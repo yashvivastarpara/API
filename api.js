12b0b5c88578002f8adc040c3aaa12e234c10622
yashvi <you@example.com>
2023-09-23 23:38:10 +0530

    yashu

---
+++ api.js
@@ -0,0 +1,19 @@
+// let getdata = async()=>{
+//     let req = await fetch("https://dummyjson.com/products")
+//     let res = await req.json()
+//     console.log(res);
+// }
+// catch(error){
+//     console.log(error);
+// }
+// try{
+//     let req = await fetch("https://dummyjson.com/products")
+// }
+
+
+fetch("https://dummyjson.com/products")
+.then((req)=>req.json())
+.then((res)=>console.log(res.products))
+.then((err)=>console.log(err))
+.finally(()=>console.log("done"));
+
